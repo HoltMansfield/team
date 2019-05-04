1e3326c1417256b5bf0686d4ce3fec1f0931996e
Holt Mansfield <HoltMansfield@gmail.com>
2019-05-03 20:54:36 -0400

    claring login

--- src/components/routes/RouteTable.jsx
+++ src/components/routes/RouteTable.jsx
@@ -9,13 +9,13 @@ import { useLoggedInUser } from 'hooks/core/use-logged-in-user/useLoggedInUser'
 import { getConfig } from 'config/secure'
 import { preloadAllRoutes } from './PreloadRoutes'
 // import { AuthorizedRoute } from './AuthorizedRoute'
-const Login = Loadable({ loader: () => import('components/core/routing/login/Login'), loading: RouteLoading })
+
 
 
 
 const ExampleComponent = () => {
   return (
-    <div><Link to="/login">Login</Link></div>
+    <div>hello</div>
   )
 }
 const config = getConfig()
@@ -30,7 +30,6 @@ function RouteTable () {
     <Switch>
       {/* Default Route */}
       <Route exact path="/" component={ExampleComponent} /> } />
-      <Route path='/login' render={(props) => <Login {...props} history={history} setLoggedInUser={setLoggedInUser} auth0Config={config.auth0} />} />
 
       {/* 404 route must be last */}
       <Route component={NotFound} />
